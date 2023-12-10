e52bddf853d12ea71f0dfb642d1bbe778f7ae2d0
Jiyoung Oh <jiyoung4061@gmail.com>
2023-12-10 17:40:49 +0900

    2023.12.10 섹션4 이전 버전

---
+++ src/main/java/hellojpa/Member_old.java
@@ -0,0 +1,90 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.Date;
+
+//@Entity
+public class Member_old {
+
+    @Id
+    private Long id;
+    @Column(name = "name")
+    private String username;
+    private Integer age;
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    /*
+    * DB에서의 Date타입은 보통 3가지로 나뉨
+    - DATE : 날짜만
+    - TIME : 시간만
+    - TIMESTAMP : 날짜 + 시간
+     */
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @Lob
+    private String description;
+
+    @Transient
+    private int temp; // 메모리에서만 계산하고 DB와는 별개로 사용할 때
+    public Member_old() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
