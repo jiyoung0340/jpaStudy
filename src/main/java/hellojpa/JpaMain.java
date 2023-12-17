54a1f60da3d3112662cf57aa455deb025703a4cb
Jiyoung Oh <jiyoung4061@gmail.com>
2023-12-17 21:45:13 +0900

    2023.12.17 섹션4 연관관계와 주인

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -14,26 +15,26 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team = new Team();
+            team.setName ("TeamA");
+            em.persist(team);
+
             Member member = new Member();
-            member.setUsername("C");
-
-            System.out.println("====================");
-            // call next value for MEMBER_SEQ 이 호출됨!
-            // 영속성 컨텍스트에 저장하기 전에 pk값을 알아야하는데 seq값도 DB가 알고있으니까 persist 하기전에 sq전략이 sequence모드이면 해당 쿼리를 호출해서 member에 id값을 세팅해줌!
-
-            /*
-            * allocationSize?
-            * 매 객체를 persist할 때마다 next val을 호출하는것은 성능에 좋지 않다!(결국 네트워크를 타는거니깐)
-            * 그러므로 allocationSize를 사용한다.
-            * allocationSize는 한번 call해 50까지 seq값을 메모리에 저장한다.
-            * 애플리케이션에서는 1부터 차근차근 객체를 저장하되, seq값이 50과 같아지면 그때 다시한번 next value를 call하고 memory에 100을 저장해 사용한다.
-            * 그렇다고 이 값을 충분히 늘리기에는 중간에 구멍이 생길 수 있으므로 적당한 값(보통 50, 100)으로 설정해 사용한다.
-            * */
+            member.setUsername("member1");
+            member.setTeam(team);
             em.persist(member);
-            System.out.println("member.id======"+member.getId()); // persist시점에 insert쿼리를 실행해 id값을 알 수 있다.
-            System.out.println("====================");
 
-            tx.commit(); // 버퍼안에 insert쿼리를 저장해뒀다가 한번에 DB에 반영이 가능!(IDENTITY는 insert를 해야만 id를 알 수 있기때문에 불가능함)
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
+            for(Member m : members){
+                System.out.println("m = " + m.getUsername());
+            }
+
+
+            tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,22 +1,23 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.util.Date;
 
 @Entity
-@SequenceGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ",
-        allocationSize = 1)
 public class Member {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
+    @Id @GeneratedValue
+    @Column (name = "MEMBER_ID")
     private Long id;
-    @Column(name = "name", nullable = false)
+
+    @Column (name = "USERNAME")
     private String username;
-    public Member() {
-    }
+
+//    @Column (name = "TEAM_ID")
+//    private Long teamId;
+
+    // 오류('Basic' attribute type should not be 'Persistence Entity' ) : 두 객체사이의 관계를 jpa에 알려줘야함!
+    @ManyToOne(fetch = FetchType.LAZY) // Member를 기준으로 하나의 팀에 여러개의 멤버가 소속되므로 ManyToOne
+    @JoinColumn (name = "TEAM_ID") // @JoinColumn(name="TEAM_ID") 를 통해 join 컬럼을 설정해줌.
+    private Team team; // Team에서 mappedBy에 이 변수명의 이름으로 연결됨!
 
     public Long getId() {
         return id;
@@ -32,5 +33,13 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
